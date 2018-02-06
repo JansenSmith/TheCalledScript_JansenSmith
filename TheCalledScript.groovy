60928dc3595abc5244f91d4ee74b56afde0ebd64
Kevin Harrington <mad.hephaestus@gmail.com>
2018-02-06 16:41:26 -0500

    gear generator

--- TheCalledScript.groovy
+++ TheCalledScript.groovy
@@ -6,11 +6,12 @@ def bevelGears = ScriptingEngine.gitScriptRun(
             "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
             "bevelGear.groovy" , // file to load
             // Parameters passed to the funcetion
-            [	  60,// Number of teeth gear a
-	            24,// Number of teeth gear b
+            [	  24,// Number of teeth gear a
+	            35,// Number of teeth gear b
 	            3.5,// thickness of gear A
 	            computeGearPitch(26.15,24),// gear pitch in arc length mm
-	            90// shaft angle, can be from 0 to 100 degrees
+	            90,// shaft angle, can be from 0 to 100 degrees
+	            0// helical angle, only used for 0 degree bevels
             ]
             )
 //Print parameters returned by the script
