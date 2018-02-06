20b45160832f82e4543cf95c650d72c46764900d
hephaestus <hephaestus@zoomzoom>
2018-02-05 19:21:49 -0500

    comments

--- TheCalledScript.groovy
+++ TheCalledScript.groovy
@@ -1,18 +1,19 @@
+// call a script from another library
 List<Object> bevelGears = (List<Object>)ScriptingEngine
 					 .gitScriptRun(
             "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
             "bevelGear.groovy" , // file to load
+            // Parameters passed to the funcetion
             [	  40,// Number of teeth gear a
 	            24,// Number of teeth gear b
 	            3.5,// thickness of gear A
 	            ((26.15/2)*((360.0)/24)*Math.PI/180*4)// gear pitch in arch length mm
-            ]// Parameters passed to the funcetion
-            );
+            ]
+            )
 //Print parameters returned by the script
 println "Bevel gear radius A " + bevelGears.get(2)
 println "Bevel gear radius B " + bevelGears.get(3)
 println "Bevel angle " + bevelGears.get(4)
 println "Bevel tooth face length " + bevelGears.get(5)
 // return the CSG parts
-
 return [bevelGears.get(0),bevelGears.get(1)]
