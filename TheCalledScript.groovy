9e3087b788e2e0a713617edfa38325fd21ac7420
Kevin Harrington <mad.hephaestus@gmail.com>
2018-02-07 20:02:34 -0500

    spelling

--- TheCalledScript.groovy
+++ TheCalledScript.groovy
@@ -6,17 +6,17 @@ def bevelGears = ScriptingEngine.gitScriptRun(
             "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
             "bevelGear.groovy" , // file to load
             // Parameters passed to the funcetion
-            [	  24,// Number of teeth gear a
-	            35,// Number of teeth gear b
+            [	  34,// Number of teeth gear a
+	            24,// Number of teeth gear b
 	            3.5,// thickness of gear A
 	            computeGearPitch(26.15,24),// gear pitch in arc length mm
-	            90,// shaft angle, can be from 0 to 100 degrees
+	           90,// shaft angle, can be from 0 to 100 degrees
 	            0// helical angle, only used for 0 degree bevels
             ]
             )
 //Print parameters returned by the script
-println "Bevel gear axil center to center " + bevelGears.get(2)
-println "Bevel gear axil Height " + bevelGears.get(3)
+println "Bevel gear axle center to center " + bevelGears.get(2)
+println "Bevel gear axle Height " + bevelGears.get(3)
 println "Bevel angle " + bevelGears.get(4)
 println "Bevel tooth face length " + bevelGears.get(5)
 // return the CSG parts
